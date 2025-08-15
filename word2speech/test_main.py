e017fe37d253fc1939aeb1c8d3173140b25a358f
Alejandro de Cora <alejandrodecora@gmail.com>
2025-08-16 00:40:31 +0200

    Fix, arreglo del test roto

--- word2speech/test_main.py
+++ word2speech/test_main.py
@@ -5,6 +5,6 @@ def test_set_config():
     fd = open("config-example.yml", encoding="utf-8")
     config = set_config(fd)
     assert isinstance(config, dict)
-    assert config.get("token") == 12345
-    assert config.get("email") == "user@domain.com"
+    assert config.get("token") == "tu_token"
+    assert config.get("email") == "tu_email@domain.com"
     assert config.get("voice") == "Alvaro"
