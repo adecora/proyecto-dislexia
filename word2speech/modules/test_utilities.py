8bd9a325dd77cf7b762ef523cf94b50a3ddb6a11
Alejandro de Cora <alejandrodecora@gmail.com>
2025-08-25 01:58:48 +0200

    Apañamos los test que fallaban

--- word2speech/modules/test_utilities.py
+++ word2speech/modules/test_utilities.py
@@ -1,15 +1,16 @@
-import pytest
 import unicodedata
 
+import pytest
+
 from .utilities import (
-    Normalizer,
     Contour,
-    validate_speed,
-    validate_pitch,
-    validate_emotion,
+    Normalizer,
     validate_bitrate,
     validate_contour_point,
+    validate_emotion,
     validate_format,
+    validate_pitch,
+    validate_speed,
 )
 
 
@@ -27,11 +28,8 @@ def test_normalizar():
 
 
 def test_entonacion_succeed():
-    contour = Contour([(10, 80), (30, 100), (60, -10)])
-    assert (
-        format(contour, "Palabra")
-        == '<prosody contour="(10%,+80%) (30%,+100%) (60%,-10%)">Palabra</prosody>'
-    )
+    contour = Contour(("10, 80", "30,100", "60,-10"))
+    assert format(contour, "Palabra") == '<prosody contour="(10%,+80%) (30%,+100%) (60%,-10%)">Palabra</prosody>'
 
 
 def test_entonacion_fail():
@@ -39,10 +37,7 @@ def test_entonacion_fail():
         Contour([(10, 80), (30, 100), (60, -10), (65, -20), (70, 0), (80, 15)])
 
     assert exec_info.type is SystemExit
-    assert (
-        exec_info.value.args[0]
-        == "error: No puede haber más de cinco puntos de entonación"
-    )
+    assert exec_info.value.args[0] == "error: No puede haber más de cinco puntos de entonación"
 
 
 def test_validate_format_success():
@@ -55,10 +50,7 @@ def test_validate_format_fail():
         fmt = "mp5"
         validate_format(fmt, CustomError)
     assert exec_info.type is CustomError
-    assert (
-        exec_info.value.args[0]
-        == f"Opción invalida: '{fmt}' (escoge entre 'mp3', 'wav', 'ogg')"
-    )
+    assert exec_info.value.args[0] == f"Opción invalida: '{fmt}' (escoge entre 'mp3', 'wav', 'ogg')"
 
 
 def test_validate_speed_success():
@@ -76,9 +68,7 @@ def test_validate_speed_fail():
         speed = 5.0
         validate_speed(speed, CustomError)
     assert exec_info.type is CustomError
-    assert (
-        exec_info.value.args[0] == f"Opción invalida: {speed} (rango entre 0.1 y 2.0)"
-    )
+    assert exec_info.value.args[0] == f"Opción invalida: {speed} (rango entre 0.1 y 2.0)"
 
 
 def test_validate_pitch_success():
@@ -109,10 +99,7 @@ def test_validate_emotion_fail():
         emotion = "bad"
         validate_emotion(emotion, CustomError)
     assert exec_info.type is CustomError
-    assert (
-        exec_info.value.args[0]
-        == f"Opción invalida: '{emotion}' (escoge entre 'evil', 'good', 'neutral')"
-    )
+    assert exec_info.value.args[0] == f"Opción invalida: '{emotion}' (escoge entre 'evil', 'good', 'neutral')"
 
 
 def test_validate_bitrate_success():
@@ -130,10 +117,7 @@ def test_validate_bitrate_fail():
         bitrate = 200_000
         validate_bitrate(bitrate, CustomError)
     assert exec_info.type is CustomError
-    assert (
-        exec_info.value.args[0]
-        == f"Opción invalida: {bitrate} (rango entre 8000 y 192000)"
-    )
+    assert exec_info.value.args[0] == f"Opción invalida: {bitrate} (rango entre 8000 y 192000)"
 
 
 def test_validate_contour_point_success():
@@ -151,15 +135,9 @@ def test_validate_contour_point_fail():
         point = "110,80"
         validate_contour_point(point, CustomError)
     assert exec_info.type is CustomError
-    assert (
-        exec_info.value.args[0]
-        == "Porcentaje tiempo invalido: 110 (rango entre 0 y 100)"
-    )
+    assert exec_info.value.args[0] == "Porcentaje tiempo invalido: 110 (rango entre 0 y 100)"
     with pytest.raises(CustomError) as exec_info:
         point = "10,180"
         validate_contour_point(point, CustomError)
     assert exec_info.type is CustomError
-    assert (
-        exec_info.value.args[0]
-        == "Porcentaje entonación invalido: 180 (rango entre -100 y 100)"
-    )
+    assert exec_info.value.args[0] == "Porcentaje entonación invalido: 180 (rango entre -100 y 100)"
